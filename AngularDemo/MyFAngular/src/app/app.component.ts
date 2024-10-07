f91d52055e988b46a89404000481e7bd125c083b
tveleza2 <93617802+tveleza2@users.noreply.github.com>
2024-10-07 11:46:40 -0500

    Angular proyect up to css pf header

--- AngularDemo/MyFAngular/src/app/app.component.ts
+++ AngularDemo/MyFAngular/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
